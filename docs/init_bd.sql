49e74f51d2a3fd72b9b245aa5de0273b301d7b57
Juliocpo946 <juliocesarp6273@gmail.com>
2025-11-13 11:11:44 -0600

    creacion de analytic, content, monitoring, recommendation

--- docs/init_bd.sql
+++ docs/init_bd.sql
@@ -1,3 +1,4 @@
 -- En MYSQL ejecutar una sola vez:
 CREATE DATABASE IF NOT EXISTS auth_service CHARACTER SET utf8mb4;
+CREATE DATABASE IF NOT EXISTS session_service CHARACTER SET utf8mb4;
 CREATE DATABASE IF NOT EXISTS cognitive_service CHARACTER SET utf8mb4;
