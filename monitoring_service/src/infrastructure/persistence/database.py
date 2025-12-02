b0b5ed5e4aa798668ae1c660f6de77048fe37f99
Juliocpo946 <juliocesarp6273@gmail.com>
2025-12-01 21:17:58 -0600

    correcciones en la bd

--- monitoring_service/src/infrastructure/persistence/database.py
+++ monitoring_service/src/infrastructure/persistence/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, declarative_base
 from src.infrastructure.config.settings import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
+engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
@@ -13,3 +13,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+    print(f"[DATABASE] [INFO] Tablas creadas exitosamente")
